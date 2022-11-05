fb78dc8554d847e42b98a52c8acc9a758adec8e6
Wllysses Tavares <wllyssestavares@gmail.com>
2022-11-05 15:45:21 -0300

    alterando nome do bg

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ const GlobalStyle = createGlobalStyle`
 
   .App {
     min-height: 100vh;
-    background: url('./src/assets/25501.jpg') center center no-repeat;
+    background: url('./src/assets/background-app.jpg') center center no-repeat;
     background-size: cover;
     display: flex;
     align-items: center;
